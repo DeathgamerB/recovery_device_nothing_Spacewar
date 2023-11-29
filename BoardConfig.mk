2a03a25d9fd02463f28f9b94754398ff29f40123
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2023-11-29 18:56:20 +0100

    Added back vibration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,3 +157,6 @@ TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_TWRPAPP := true
 TW_OVERRIDE_SYSTEM_PROPS := "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+
+# Vibrator
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
